eb1e05cd0f08ee8fd2e740bcc8bbef994deb929d
Ashish <kapoleashish.76@gmail.com>
2021-01-01 19:46:31 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,109 @@
+const router = require("express").Router();
+// Bring in the User Registration function
+const {
+  userAuth,
+  userLogin,
+  checkRole,
+  userRegister,
+  serializeUser
+} = require("../utils/Auth");
+
+// Users Registeration Route
+router.post("/register-user", async (req, res) => {
+  await userRegister(req.body, "user", res);
+});
+
+// Admin Registration Route
+router.post("/register-admin", async (req, res) => {
+  await userRegister(req.body, "admin", res);
+});
+
+// Super Admin Registration Route
+router.post("/register-super-admin", async (req, res) => {
+  await userRegister(req.body, "superadmin", res);
+});
+
+// employee Registration Route
+router.post("/register-employee", async (req, res) => {
+  await userRegister(req.body, "employee", res);
+});
+
+
+// Registration are completed now Login routes..
+
+// Users Login Route
+router.post("/login-user", async (req, res) => {
+  await userLogin(req.body, "user", res);
+});
+
+// Admin Login Route
+router.post("/login-admin", async (req, res) => {
+  await userLogin(req.body, "admin", res);
+});
+
+// Super Admin Login Route
+router.post("/login-super-admin", async (req, res) => {
+  await userLogin(req.body, "superadmin", res);
+});
+
+
+// employee Login Route
+router.post("/login-employee", async (req, res) => {
+  await userLogin(req.body, "employee", res);
+});
+
+
+// Login Routes are made now we need to protect each route 
+
+
+// Users Protected Route
+router.get(
+  "/user-protectd",
+  userAuth,
+  checkRole(["user"]),
+  async (req, res) => {
+    return res.json("Hello User");
+  }
+);
+
+// Admin Protected Route
+router.get(
+  "/admin-protectd",
+  userAuth,
+  checkRole(["admin"]),
+  async (req, res) => {
+    return res.json("Hello Admin");
+  }
+);
+
+// Super Admin Protected Route
+router.get(
+  "/super-admin-protectd",
+  userAuth,
+  checkRole(["superadmin"]),
+  async (req, res) => {
+    return res.json("Hello Super Admin");
+  }
+);
+
+// Super Admin Protected Route
+router.get(
+  "/super-admin-and-admin-protectd",
+  userAuth,
+  checkRole(["superadmin", "admin"]),
+  async (req, res) => {
+    return res.json("Super admin and Admin");
+  }
+);
+
+// Super Admin Protected Route
+router.get(
+  "/employee-protectd",
+  userAuth,
+  checkRole(["employee"]),
+  async (req, res) => {
+    return res.json("Hello Super Employee");
+  }
+);
+
+module.exports = router;
